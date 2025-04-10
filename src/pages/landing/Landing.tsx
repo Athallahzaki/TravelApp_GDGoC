7419fcc73094f7311853ba2aa8dcce9933e050be
Athallahzaki <zapaweh20@gmail.com>
2025-04-10 23:07:48 +0700

    feat: Landing Template Add Dashboard Button

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
 const Landing = () => {
   return (
-    <div>
-      Landing Page
+    <div className="flex h-screen w-screen items-center justify-center text-center">
+      <div className="grid grid-rows-2">
+        <p>Landing Page</p>
+        <Button asChild>
+          <Link to="/dashboard">Go To Dashboard</Link>  
+        </Button>
+      </div>
     </div>
   )
 };
